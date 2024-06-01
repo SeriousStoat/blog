44733831f8158a96ac9e6b61f268e1210187396b
SeriousStoat <aseriousstoat@pm.me>
2024-06-01 12:26:37 -0700

    added models to admin.py register

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog_app.models import Category, Comment, Post
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
