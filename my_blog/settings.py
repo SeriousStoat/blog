699f1452dff8297b22e395840f35f0bf877103bc
SeriousStoat <aseriousstoat@pm.me>
2024-06-01 11:01:08 -0700

    Initialized Database for blog_app, created a Django admin profile, and fixed python error by adding tzdata modual to the virtual enviroment

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_app.apps.BlogAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog_app.app.BlogConfig',
 ]
 
 MIDDLEWARE = [
