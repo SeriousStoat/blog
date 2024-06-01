9240ad3ffc29fcef12e76f627d92bff979dde8b9
SeriousStoat <aseriousstoat@pm.me>
2024-06-01 12:48:59 -0700

    fixed titles in the models.py file

--- blog_app/models.py
+++ blog_app/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    class Meta:
+        verbose_name_plural = "catagories"
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
@@ -12,8 +18,14 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="post")
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
