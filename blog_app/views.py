5cbf28bdbd213a5fc86b5f53a7d28ca786e17460
SeriousStoat <aseriousstoat@pm.me>
2024-06-01 21:48:24 -0700

    Created Tamplate pages catergory.html, detail.html, and index.html

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from blog_app.models import Post, Comment
 
 # Create your views here.
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("created_on")
+    context = {
+        "category":category,
+        "posts": posts,
+    }
+    return render(request, "blog/category.html", context)
+
+def blog_details(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context =  {
+        "post":post,
+        "comments":comments,
+    }
+
+    return render(request, "blog/detail.html", context)
