81b71c77b6369720c650f84947dd2df3e1874ee7
SeriousStoat <aseriousstoat@pm.me>
2024-06-04 12:28:21 -0700

    fixed typo that was breaking the 'category' system; template files are all working properly now.

--- blog_app/models.py
+++ blog_app/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     name = models.CharField(max_length=30)
 
     class Meta:
-        verbose_name_plural = "catagories"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
-    path("catergory/<catergory>/", views.blog_category, name="blog_catergory"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
 ]
