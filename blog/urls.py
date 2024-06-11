eae7ce6276d244a6a179ee3236d574b1d0321985
SeriousStoat <aseriousstoat@pm.me>
2024-06-11 10:52:25 -0700

    Created Nav Menu, updated styles, added 'about' page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("about", views.blog_about, name="blog_about"),
     path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
